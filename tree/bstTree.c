9799959839d408bc70e548c13f00838713e6b932
Zhou, Alex-Y <Alex-Y.Zhou@aia.com>
2020-10-16 18:09:06 +0800

    kkkk

--- tree/bstTree.c
+++ tree/bstTree.c
@@ -8,12 +8,12 @@ typedef struct BiTNode {
     struct BiTNode *lchild, *rchild;
 } BiTNode, *BiTree;
 
-void insertNode(BiTree tree,int data){
+void insertNode(BiTree tree, int data) {
 
     //二叉树为空 插入元素为根节点
-    if(tree == NULL){
+    if (tree == NULL) {
 
-        tree = (BiTree)malloc(sizeof(BiTNode));
+        tree = (BiTree) malloc(sizeof(BiTNode));
         tree->data = data;
         tree->rchild = NULL;
         tree->lchild = NULL;
@@ -22,36 +22,85 @@ void insertNode(BiTree tree,int data){
     }
 
     //出现相同元素 返回
-    if(tree->data == data){
+    if (tree->data == data) {
         return;
     }
 
     //插入左子树
-    if(data < tree->data){
-        insertNode(tree->lchild,data);
+    if (data < tree->data) {
+        insertNode(tree->lchild, data);
     }
 
     //插入右子树
-    if(data > tree->data){
-        insertNode(tree->rchild,data);
+    if (data > tree->data) {
+        insertNode(tree->rchild, data);
     }
 }
 
-int main(){
+BiTNode *searchNode1(BiTree tree, int data) {
+
+    if (tree == NULL) {
+        return NULL;
+    }
+
+    if (tree->data == data) {
+        return tree;
+    }
+
+    //查找左子树
+    if (data < tree->data) {
+        return searchNode1(tree->lchild, data);
+    }
+    //查找右子树
+    if (data > tree->data) {
+        return searchNode1(tree->rchild, data);
+    }
+
+}
+
+BiTNode *searchNode2(BiTree tree, int data) {
+
+    BiTNode *p = tree;
+    if (p == NULL) {
+        return NULL;
+    }
+
+    while (p != NULL) {
+        if (p->data == data) {
+            return p;
+        }
+
+        if (data < p->data) {
+            p = p->lchild;
+        }
+        if (data > p->data) {
+            p = p->rchild;
+        }
+    }
+
+    return NULL;
+}
+
+void deleteNode(BiTree tree, int data) {
+
+    //查找关键字为data的待删除节点
+}
+
+int main() {
 
     BiTree tree = NULL;
 
-    srand((unsigned)time(NULL));
-    for (int i=0;i<10;i++){
-       int data = rand()%10;
-       if(i == 0){
-           tree = (BiTree)malloc(sizeof(BiTNode));
-           tree->data = data;
-           tree->lchild = NULL;
-           tree->rchild = NULL;
-       }
+    srand((unsigned) time(NULL));
+    for (int i = 0; i < 10; i++) {
+        int data = rand() % 10;
+        if (i == 0) {
+            tree = (BiTree) malloc(sizeof(BiTNode));
+            tree->data = data;
+            tree->lchild = NULL;
+            tree->rchild = NULL;
+        }
 
-       insertNode(tree,data);
+        insertNode(tree, data);
     }
 
     return 0;
